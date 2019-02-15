f5ba5f335eb249fb50e28736a489cf6b7e0cfcee
Samuel Ng <eclmist@yahoo.com>
2019-02-15 20:27:07 +0800

    Added basic unlit hlsl shader

---
+++ Assets/Graphics/Unlit.hlsl
@@ -0,0 +1,31 @@
+﻿#ifndef MYST_UNLIT_INCLUDED
+#define MYST_UNLIT_INCLUDED
+
+float4x4 unity_MatrixVP;
+float4x4 unity_ObjectToWorld;
+
+struct VS_INPUT
+{
+    float4 pos : POSITION;
+};
+
+struct VS_OUTPUT
+{
+    float4 clipPos : SV_POSITION;
+};
+
+VS_OUTPUT VS_Unlit(const VS_INPUT v)
+{
+    VS_OUTPUT output;
+    float4 worldPos = mul(unity_ObjectToWorld, float4(v.pos.xyz, 1.0));
+    output.clipPos = mul(unity_MatrixVP, worldPos);
+
+    return output;
+}
+
+float4 PS_Unlit(const VS_OUTPUT input) : SV_TARGET
+{
+    return 1;
+}
+
+#endif // MYST_UNLIT_INCLUDED
---
+++ Assets/Graphics/Unlit.shader
@@ -0,0 +1,18 @@
+﻿Shader "Myst/Unlit" {
+    Properties {}
+
+    SubShader 
+    {
+        Pass 
+        {
+            HLSLPROGRAM
+            
+            #pragma vertex VS_Unlit
+            #pragma fragment PS_Unlit
+
+            #include "Unlit.hlsl"
+
+            ENDHLSL
+        }
+    }
+}
